7cf35b473433c19933673f1ac34faa14b99d41d2
HikaruEgashira <shighten@gmail.com>
2020-06-15 19:46:52 +0900

    del

--- wasi-demo/src/main.rs
+++ wasi-demo/src/main.rs
@@ -1,26 +1,10 @@
 use std::env;
 use std::fs;
-use std::io::{Read, Write};
 
 #[macro_use] // macroを使うのでmacro_useを追記
 extern crate clap;
 
 use clap::{App, Arg};
-
-fn process(input_fname: &str, output_fname: &str) -> Result<(), String> {
-    let mut input_file =
-        fs::File::open(input_fname).map_err(|err| format!("error opening input: {}", err))?;
-    let mut contents = Vec::new();
-    input_file
-        .read_to_end(&mut contents)
-        .map_err(|err| format!("read error: {}", err))?;
-
-    let mut output_file = fs::File::create(output_fname)
-        .map_err(|err| format!("error opening output '{}': {}", output_fname, err))?;
-    output_file
-        .write_all(&contents)
-        .map_err(|err| format!("write error: {}", err))
-}
 
 fn main() {
     let app = App::new(crate_name!()) // Cargo.tomlのnameを参照する
