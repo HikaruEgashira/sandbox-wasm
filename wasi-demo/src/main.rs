9c699479dbc2e607c3101512ff23e9b18f959b21
HikaruEgashira <shighten@gmail.com>
2020-06-15 19:32:26 +0900

    ls

--- wasi-demo/src/main.rs
+++ wasi-demo/src/main.rs
@@ -2,6 +2,11 @@ use std::env;
 use std::fs;
 use std::io::{Read, Write};
 
+#[macro_use] // macroを使うのでmacro_useを追記
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn process(input_fname: &str, output_fname: &str) -> Result<(), String> {
     let mut input_file =
         fs::File::open(input_fname).map_err(|err| format!("error opening input: {}", err))?;
@@ -18,15 +23,29 @@ fn process(input_fname: &str, output_fname: &str) -> Result<(), String> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
+    let app = App::new(crate_name!()) // Cargo.tomlのnameを参照する
+        .version(crate_version!()) // Cargo.tomlのversionを参照する
+        .author(crate_authors!()) // Cargo.tomlのauthorsを参照する
+        .about(crate_description!()) // Cargo.tomlのdescriptionを参照する
+        .arg(Arg::with_name("path"));
 
-    if args.len() < 3 {
-        eprintln!("{} <input_file> <output_file>", program);
-        return;
-    }
+    // 引数を解析
+    let matches = app.get_matches();
 
-    if let Err(err) = process(&args[1], &args[2]) {
-        eprintln!("{}", err)
+    // pathが指定されていれば値を表示
+    if let Some(o) = matches.value_of("path") {
+        ls(o).iter().for_each(|f| println!("{}", f));
     }
+}
+
+fn ls(base: &str) -> Vec<String> {
+    let dir = fs::read_dir(base).unwrap();
+
+    let mut entry = dir
+        .map(|f| f.unwrap().path().to_str().unwrap().to_string())
+        .collect::<Vec<String>>();
+    entry.sort();
+    let entry = entry; // immutable にする
+
+    entry
 }
